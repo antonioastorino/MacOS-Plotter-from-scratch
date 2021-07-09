e57f14eeac3a779c446c6616d8640cf72a25b0b8
Antonio Astorino <antonio.astorino.phd@gmail.com>
2021-07-09 22:13:01 +0200

    Added white test view

--- bin/run.sh
+++ bin/run.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 set -e
-BD="`pwd`"
+BD="$(pwd)"
 echo "Building"
 ${BD}/bin/build.sh
 echo "Registering"
 /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -f "$BD/plotter.app"
+
+echo "Closing running instance"
+PID=$(ps aux | grep plotter.app/Contents/MacOS/plotter | grep -v grep | awk '{print $2}')
+if ! [ -x ${PID} ]; then
+	kill ${PID}
+fi
+
 echo "Running"
 open $BD/plotter.app
 tail -f /Volumes/DataMBP/plotter.log
--- src/main.mm
+++ src/main.mm
@@ -22,64 +22,69 @@ int main(int argc, const char* argv[])
 {
     FILE* log_file = fopen("/Volumes/DataMBP/plotter.log", "w");
 
-    @autoreleasepool
+    // Retrieve the screen coordinates of the main screen
+    NSRect screenRect = [[NSScreen mainScreen] frame];
+
+    // Create a rect object to save position and size of the window we are creating
+    NSRect windowRect = NSMakeRect(screenRect.size.width / 4.0f, screenRect.size.height / 4.0f,
+                                   screenRect.size.width / 2.0f, screenRect.size.height / 2.0f);
+
+    // Set window style parameters
+    NSWindowStyleMask windowStyleMask = NSWindowStyleMaskClosable | NSWindowStyleMaskTitled;
+
+    // https://stackoverflow.com/questions/15694510/programmatically-create-initial-window-of-cocoa-app-os-x
+
+    // NSApplication - AppKit | Apple Developer Documentation
+
+    NSApplication* myApplication = [NSApplication sharedApplication];
+
+    NSRect viewFrame   = NSMakeRect(100, 100, 50, 50);
+    NSView* newView    = [[NSView alloc] initWithFrame:viewFrame];
+    newView.wantsLayer = TRUE;
+    [newView.layer setBackgroundColor:[NSColor colorWithCalibratedRed:1.0f
+                                                                green:1.0f
+                                                                 blue:1.0
+                                                                alpha:1.0f]
+                                          .CGColor];
+    //		NSArray *myTopLevelObjects;
+    //		[[NSBundle mainBundle] loadNibNamed:@"myNib" // NOTE: mainBundle is a
+    // method!!
+    // owner:myApplication topLevelObjects:&myTopLevelObjects];
+
+    // Create a window
+    // https://stackoverflow.com/questions/314256/how-do-i-create-a-cocoa-window-programmatically
+    NSWindow* window = [[NSWindow alloc] initWithContentRect:windowRect
+                                                   styleMask:windowStyleMask
+                                                     backing:NSBackingStoreBuffered
+                                                       defer:YES];
+
+    [window setBackgroundColor:[NSColor blueColor]];
+    [window makeKeyAndOrderFront:nil];
+    [window setTitle:@"plotter"];
+    [[window contentView] addSubview:newView];
+
+    NSLog(@"%@", [[NSApplication sharedApplication] mainWindow]);
+
+    appWindowDelegate* windowDelegate = [[appWindowDelegate alloc] init];
+    [window setDelegate:windowDelegate];
+
+    NSEvent* event;
+    while (appRunning)
     {
-        // Retrieve the screen coordinates of the main screen
-        NSRect screenRect = [[NSScreen mainScreen] frame];
-
-        // Create a rect object to save position and size of the window we are creating
-        NSRect windowRect = {screenRect.size.width / 4.0f, screenRect.size.height / 4.0f,
-                             screenRect.size.width / 2.0f, screenRect.size.height / 2.0f
-
-        };
-
-        // Set window style parameters
-        NSWindowStyleMask windowStyleMask = NSWindowStyleMaskClosable | NSWindowStyleMaskTitled;
-
-        // https://stackoverflow.com/questions/15694510/programmatically-create-initial-window-of-cocoa-app-os-x
-
-        // NSApplication - AppKit | Apple Developer Documentation
-
-        NSApplication* myApplication = [NSApplication sharedApplication];
-
-        //		NSArray *myTopLevelObjects;
-        //		[[NSBundle mainBundle] loadNibNamed:@"myNib" // NOTE: mainBundle is a
-        // method!!
-        // owner:myApplication topLevelObjects:&myTopLevelObjects];
-
-        // Create a window
-        // https://stackoverflow.com/questions/314256/how-do-i-create-a-cocoa-window-programmatically
-        NSWindow* window = [[NSWindow alloc] initWithContentRect:windowRect
-                                                       styleMask:windowStyleMask
-                                                         backing:NSBackingStoreBuffered
-                                                           defer:YES];
-
-        [window setBackgroundColor:[NSColor blueColor]];
-        [window makeKeyAndOrderFront:nil];
-        [window setTitle:@"plotter"];
-        NSLog(@"%@", [[NSApplication sharedApplication] mainWindow]);
-
-        appWindowDelegate* windowDelegate = [[appWindowDelegate alloc] init];
-        [window setDelegate:windowDelegate];
-
-        NSEvent* event;
-        while (appRunning)
+        do
         {
-            do
+            event = [NSApp nextEventMatchingMask:NSEventMaskAny
+                                       untilDate:nil
+                                          inMode:NSDefaultRunLoopMode
+                                         dequeue:YES];
+            if ([event type] != 0)
             {
-                event = [NSApp nextEventMatchingMask:NSEventMaskAny
-                                           untilDate:nil
-                                              inMode:NSDefaultRunLoopMode
-                                             dequeue:YES];
-                if ([event type] != 0)
-                {
-                    fprintf(log_file, "%lu ", [event type]);
-                    fflush(log_file);
-                }
-                [NSApp sendEvent:event];
-            } while (event != nil);
-        }
-        fprintf(log_file, "%s\n", "Goodbye");
+                fprintf(log_file, "%lu ", [event type]);
+                fflush(log_file);
+            }
+            [NSApp sendEvent:event];
+        } while (event != nil);
     }
+    fprintf(log_file, "%s\n", "Goodbye");
     return 0;
 }
